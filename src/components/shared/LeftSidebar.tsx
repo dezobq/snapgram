fdf892a7856bc41b04d6f29802f3f283b84dc381
dezobq <84927483+dezobq@users.noreply.github.com>
2023-11-15 11:39:10 -0500

    implemented the PostCard, Like, unLike, save and unsave post. Going to edit post now.

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -45,7 +45,7 @@ const LeftSidebar = () => {
             const isActive = pathname === link.route;
             return (
               <li key={link.label} className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`}>
-                <NavLink to={link.route} className="flex gap-4 items-center">
+                <NavLink to={link.route} className="flex gap-4 items-center p-4">
                   <img
                     className={`group-hover:invert-white ${
                       isActive && "invert-white"
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -6,7 +6,7 @@ import {
   useInfiniteQuery,
 
 } from '@tanstack/react-query'
-import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
+import { createPost, createUserAccount, deleteSavedPost, getCurrentUser, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '../appwrite/api'
 import { QUERY_KEYS } from './queryKeys'
 
 
@@ -46,4 +46,82 @@ export const useSignOutAccount = () => {
         });
       },
     })
+  }
+
+  export const useGetRecentPosts = () => {
+    return useQuery({
+      queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      queryFn: getRecentPosts,
+    });
+  }
+
+  export const useLikePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: ({
+        postId,
+        likesArray,
+      }: {
+        postId: string;
+        likesArray: string[];
+      }) => likePost(postId, likesArray),
+      onSuccess: (data) => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        });
+      },
+    });
+  };
+
+  export const useSavePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: ({ userId, postId }: { userId: string; postId: string }) =>
+        savePost(userId, postId),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        });
+      },
+    });
+  };
+  
+  export const useDeleteSavedPost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (savedRecordId: string) => deleteSavedPost(savedRecordId),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        });
+      },
+    });
+  };
+
+  export const useGetCurrentUser = () => {
+    return useQuery({
+      queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+      queryFn: getCurrentUser
+    });
   }
