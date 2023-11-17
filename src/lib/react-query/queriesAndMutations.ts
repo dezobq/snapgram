8c1f09115ec139dde4a49e931a43be9d7ffc2482
dezobq <84927483+dezobq@users.noreply.github.com>
2023-11-17 16:01:56 -0500

    IUpdateUser bug

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -176,7 +176,7 @@ export const useDeletePost = () => {
 export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts as any,
+    queryFn: getInfinitePosts,
     getNextPageParam: (lastPage: any) => {
       if (lastPage && lastPage.documents.length === 0) {
         return null;
